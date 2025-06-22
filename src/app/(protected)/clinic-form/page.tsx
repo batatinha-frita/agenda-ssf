6d9d3b7a8d1e8e2083c45d33d1527ec749ac91a5
José Gabriel Resende de Souza <josegabrielresendedesouza@gmail.com>
2025-06-22 11:02:42 -0300

    feat: ensure user has clinic on "main"

--- src/app/(protected)/clinic-form/page.tsx
+++ src/app/(protected)/clinic-form/page.tsx
@@ -7,8 +7,17 @@ import {
 } from "@/components/ui/dialog";
 
 import ClinicForm from "./_components/form";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-const ClinicFormPage = () => {
+const ClinicFormPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
   return (
     <div>
       <Dialog open>
--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -9,8 +9,21 @@ import {
 } from "@/components/ui/page-container";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const DoctorsPage = () => {
+const DoctorsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+  if (!session.user.clinic) {
+    redirect("/clinic-form");
+  }
+
   return (
     <PageContainer>
       <PageHeader>
